daab0a73572a59ef3bee6cb80e5f864f28681940
sasikumar <sasikumar@interloid.com>
2026-05-08 17:35:34 +0530

    Add password length and review changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable,:confirmable, :lockable, :timeoutable, :trackable
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, if: :password_present?
+  validate :password_must_contain_special_characters
+  
+  private 
+  def password_present?
+    password.present?
+  end
+
+  def password_must_contain_special_characters
+    return if password.blank?
+
+    unless password.match?(/[^A-Za-z0-9]/)
+      errors.add(:password, "must contain at least one specail character")
+    end
+  end
 end
