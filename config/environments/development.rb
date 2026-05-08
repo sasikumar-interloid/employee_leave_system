a91f6d6b56df7d18b353796d6dd36ac66c6aff65
sasikumar <sasikumar@interloid.com>
2026-05-08 16:04:00 +0530

    fix review changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,7 +77,6 @@ Rails.application.configure do
   # config.generators.apply_rubocop_autocorrect_after_generate!
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_06_130141) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_08_102740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -34,7 +34,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_06_130141) do
     t.string "unconfirmed_email"
     t.string "unlock_token"
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 end
