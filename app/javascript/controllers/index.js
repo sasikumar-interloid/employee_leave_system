134d7019594a1bc950e93f955813b257cb6494dd
sasikumar <sasikumar@interloid.com>
2026-05-09 15:31:10 +0530

    Add dropdown

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import DropdownController from "./dropdown_controller"
 
-application.register("dropdown", DropdownController)
 eagerLoadControllersFrom("controllers", application)
